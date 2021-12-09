8683079c66d05514572b19365b4dd632a4cdb9dd
Drew Camp <53627062+DqwertyC@users.noreply.github.com>
2021-12-09 10:19:31 -0800

    2021 Day 9

--- src/Puzzles/2021/CSharp/Solution_2021_09.cs
+++ src/Puzzles/2021/CSharp/Solution_2021_09.cs
@@ -5,12 +5,85 @@ namespace AdventOfCode.Solutions
 {
   public class Solution_2021_09 : CSharpSolution
   {
+    public static (int, int)[] neighbors = { (1, 0), (0, 1), (-1, 0), (0, -1) };
+
+    string test = @"2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+
     public override void Solve(PuzzleInput input)
     {
-      // Write your puzzle solution here!
+      PuzzleInput testInput = new PuzzleInput(test);
+      var map = input.GetDictionary();
+      List<(int, int)> lowPoints = new List<(int, int)>();
 
-      SubmitPartOne(string.Empty);
-      SubmitPartTwo(string.Empty);
+      foreach ((int x, int y) key in map.Keys)
+      {
+        bool isLowPoint = true;
+        foreach ((int x, int y) dir in neighbors)
+        {
+          (int x, int y) search = (key.x + dir.x, key.y + dir.y);
+
+          if (map.ContainsKey(search) && map[search] <= map[key])
+          {
+            isLowPoint = false;
+          }
+        }
+
+        if (isLowPoint)
+        {
+          lowPoints.Add(key);
+        }
+      }
+
+      int sum = lowPoints.Count;
+      foreach ((int, int) key in lowPoints)
+      {
+        sum += (map[key] - '0');
+      }
+
+      SubmitPartOne(sum);
+
+      List<int> basinSizes = new List<int>();
+
+      foreach ((int, int) key in lowPoints)
+      {
+        HashSet<(int, int)> inBasin = new HashSet<(int, int)>();
+        Queue<(int, int)> toVisit = new Queue<(int, int)>();
+        inBasin.Add(key);
+        toVisit.Enqueue(key);
+
+        while (toVisit.Count > 0)
+        {
+          (int x, int y) current = toVisit.Dequeue();
+
+          foreach ((int x, int y) dir in neighbors)
+          {
+            (int, int) search = (current.x + dir.x, current.y + dir.y);
+
+            if (!inBasin.Contains(search) && map.ContainsKey(search) && map[search] != '9')
+            {
+              inBasin.Add(search);
+              toVisit.Enqueue(search);
+            }
+          }
+        }
+
+        basinSizes.Add(inBasin.Count);
+      }
+
+      basinSizes.Sort();
+
+      long product = 1;
+
+      for (int i = 1; i <= 3; i++)
+      {
+        product *= basinSizes[basinSizes.Count - i];
+      }
+
+      SubmitPartTwo(product);
     }
   }
 }
