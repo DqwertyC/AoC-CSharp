0cf3415a663cadb9d89973f8722556067a1d495e
Drew Camp <53627062+DqwertyC@users.noreply.github.com>
2021-12-10 11:51:40 -0800

    Day 10

--- src/Puzzles/2021/CSharp/Solution_2021_10.cs
+++ src/Puzzles/2021/CSharp/Solution_2021_10.cs
@@ -5,12 +5,85 @@ namespace AdventOfCode.Solutions
 {
   public class Solution_2021_10 : CSharpSolution
   {
+
+    private Dictionary<char, char> chunkCloser;
+    private Dictionary<char, int> corruptScores;
+    private Dictionary<char, int> incompleteScores;
+
     public override void Solve(PuzzleInput input)
     {
-      // Write your puzzle solution here!
+      chunkCloser = new Dictionary<char, char>();
+      chunkCloser['('] = ')';
+      chunkCloser['['] = ']';
+      chunkCloser['{'] = '}';
+      chunkCloser['<'] = '>';
 
-      SubmitPartOne(string.Empty);
-      SubmitPartTwo(string.Empty);
+      corruptScores = new Dictionary<char, int>();
+      corruptScores[')'] = 3;
+      corruptScores[']'] = 57;
+      corruptScores['}'] = 1197;
+      corruptScores['>'] = 25137;
+
+      incompleteScores = new Dictionary<char, int>();
+      incompleteScores[')'] = 1;
+      incompleteScores[']'] = 2;
+      incompleteScores['}'] = 3;
+      incompleteScores['>'] = 4;
+
+      long corruptSum = 0;
+      List<long> validScores = new List<long>();
+      foreach (string line in input.GetLines())
+      {
+        if (GetScore(line, out long score))
+        {
+          corruptSum += score;
+        }
+        else
+        {
+          validScores.Add(score);
+        }
+      }
+
+      SubmitPartOne(corruptSum);
+      validScores.Sort();
+      SubmitPartTwo(validScores[validScores.Count / 2]);
+    }
+
+    private bool GetScore(string line, out long score)
+    {
+      Stack<char> closerStack = new Stack<char>();
+
+      for (int i = 0; i < line.Length; i++)
+      {
+        char c = line[i];
+
+        if (chunkCloser.ContainsKey(c))
+        {
+          closerStack.Push(chunkCloser[c]);
+        }
+        else
+        {
+          if (closerStack.Peek() == c)
+          {
+            closerStack.Pop();
+          }
+          else
+          {
+            score = corruptScores[c];
+            return true;
+          }
+        }
+      }
+
+      score = 0;
+      while (closerStack.Count > 0)
+      {
+        score *= 5;
+        score += incompleteScores[closerStack.Pop()];
+      }
+
+      return false;
+
     }
   }
 }
