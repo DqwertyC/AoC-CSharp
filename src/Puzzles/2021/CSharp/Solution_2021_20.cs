f35fd64466fa6bf88c996c904ce9f4f0c70ce076
Drew Camp <53627062+DqwertyC@users.noreply.github.com>
2021-12-23 17:04:14 -0800

    Days 21 and 22

--- src/Puzzles/2021/CSharp/Solution_2021_20.cs
+++ src/Puzzles/2021/CSharp/Solution_2021_20.cs
@@ -1,5 +1,7 @@
 using AdventOfCode.Utils;
 using System.Collections.Generic;
+using System;
+using System.Text;
 
 namespace AdventOfCode.Solutions
 {
@@ -7,10 +9,202 @@ namespace AdventOfCode.Solutions
   {
     public override void Solve(PuzzleInput input)
     {
-      // Write your puzzle solution here!
+      var chunks = input.GetChunks();
 
-      SubmitPartOne(string.Empty);
-      SubmitPartTwo(string.Empty);
+      List<bool> lookupTable = new List<bool>();
+      foreach (char c in chunks[0][0])
+      {
+        lookupTable.Add(c == '#');
+      }
+
+      Dictionary<Coordinate2D, bool> image = new Dictionary<Coordinate2D, bool>();
+
+      int x = 0;
+      int y = 0;
+
+      foreach (string line in chunks[1])
+      {
+        x = 0;
+        foreach (char c in line)
+        {
+          image[(x, y)] = c == '#';
+          x++;
+        }
+        y++;
+      }
+
+      image = AddNeighbors(image);
+      image = AddNeighbors(image);
+
+      image = UpdateImage(image, lookupTable, false);
+      image = UpdateImage(image, lookupTable, false);
+
+      int onCount = 0;
+      foreach (bool b in image.Values)
+      {
+        onCount += b ? 1 : 0;
+      }
+
+      SubmitPartOne(onCount);
+
+      for (int i = 0; i < 24; i++)
+      {
+        image = UpdateImage(image, lookupTable, false);
+        image = UpdateImage(image, lookupTable, false);
+      }
+
+      onCount = 0;
+      foreach (bool b in image.Values)
+      {
+        onCount += b ? 1 : 0;
+      }
+
+      Console.Out.WriteLine(DisplayImage(image));
+
+      SubmitPartTwo(onCount);
+    }
+
+    private string DisplayImage(Dictionary<Coordinate2D, bool> image)
+    {
+      List<Coordinate2D> mutableKeys = new List<Coordinate2D>();
+      mutableKeys.AddRange(image.Keys);
+      mutableKeys.Sort();
+
+      StringBuilder sb = new StringBuilder();
+
+      for (int y = mutableKeys[0].y; y <= mutableKeys[mutableKeys.Count - 1].y; y++)
+      {
+        for (int x = mutableKeys[0].x; x <= mutableKeys[mutableKeys.Count - 1].x; x++)
+        {
+          sb.Append(image.GetValueOrDefault((x, y), false) ? '#' : '.');
+        }
+        sb.Append('\n');
+      }
+
+      return sb.ToString();
+
+    }
+
+    private Dictionary<Coordinate2D, bool> AddNeighbors(Dictionary<Coordinate2D, bool> image)
+    {
+      Dictionary<Coordinate2D, bool> newImage = new Dictionary<Coordinate2D, bool>();
+      HashSet<Coordinate2D> nearby = new HashSet<Coordinate2D>();
+
+      foreach (Coordinate2D coord in image.Keys)
+      {
+        newImage[coord] = image[coord];
+
+        foreach (Coordinate2D neighbor in Coordinate2D.neighborsDiagonal)
+        {
+          if (!image.ContainsKey(coord + neighbor))
+          {
+            newImage[coord + neighbor] = false;
+          }
+        }
+      }
+
+      return newImage;
+    }
+
+    private Dictionary<Coordinate2D, bool> UpdateImage(Dictionary<Coordinate2D, bool> oldImage, List<bool> lookupTable, bool unknowns)
+    {
+      Dictionary<Coordinate2D, bool> newImage = new Dictionary<Coordinate2D, bool>();
+      HashSet<Coordinate2D> newTiles = new HashSet<Coordinate2D>();
+
+      foreach (Coordinate2D coord in oldImage.Keys)
+      {
+        newImage[coord] = lookupTable[GetIndex(oldImage, coord, unknowns)];
+
+        foreach (Coordinate2D neighbor in Coordinate2D.neighborsDiagonal)
+        {
+          if (!oldImage.ContainsKey(coord + neighbor))
+          {
+            newImage[coord + neighbor] = lookupTable[GetIndex(oldImage, coord + neighbor, unknowns)];
+
+          }
+        }
+      }
+
+      return newImage;
+    }
+
+    private int GetIndex(Dictionary<Coordinate2D, bool> image, Coordinate2D center, bool unknowns)
+    {
+      int index = 0;
+      index += image.GetValueOrDefault(center + (-1, -1), unknowns) ? 1 : 0; index <<= 1;
+      index += image.GetValueOrDefault(center + (0, -1), unknowns) ? 1 : 0; index <<= 1;
+      index += image.GetValueOrDefault(center + (+1, -1), unknowns) ? 1 : 0; index <<= 1;
+      index += image.GetValueOrDefault(center + (-1, 0), unknowns) ? 1 : 0; index <<= 1;
+      index += image.GetValueOrDefault(center + (0, 0), unknowns) ? 1 : 0; index <<= 1;
+      index += image.GetValueOrDefault(center + (+1, 0), unknowns) ? 1 : 0; index <<= 1;
+      index += image.GetValueOrDefault(center + (-1, +1), unknowns) ? 1 : 0; index <<= 1;
+      index += image.GetValueOrDefault(center + (0, +1), unknowns) ? 1 : 0; index <<= 1;
+      index += image.GetValueOrDefault(center + (+1, +1), unknowns) ? 1 : 0;
+
+      return index;
+    }
+
+    private class Coordinate2D : IComparable<Coordinate2D>
+    {
+      public int x;
+      public int y;
+
+      public static Coordinate2D[] neighborsLinear = { (1, 0), (0, 1), (-1, 0), (0, -1) };
+      public static Coordinate2D[] neighborsDiagonal = { (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1) };
+
+      public Coordinate2D(int x, int y)
+      {
+        this.x = x;
+        this.y = y;
+      }
+
+      public Coordinate2D Rotate(int rz)
+      {
+        (int x, int y) transformed = (x, y);
+
+        for (int i = 0; i < rz % 4; i++)
+        {
+          int temp = transformed.x;
+          transformed.x = -transformed.y;
+          transformed.y = temp;
+        }
+
+        return transformed;
+      }
+
+      public override string ToString()
+      {
+        return "(" + x + "," + y + ")";
+      }
+
+      public int CompareTo(Coordinate2D other)
+      {
+        int comparison = this.y - other.y;
+        if (comparison != 0) return Math.Sign(comparison);
+        else return Math.Sign(this.x - other.x);
+      }
+
+      public override bool Equals(Object other)
+      {
+        if (!other.GetType().Equals(this.GetType()))
+          return false;
+
+        Coordinate2D o = (Coordinate2D)other;
+        return (x == o.x && y == o.y);
+      }
+
+      public override int GetHashCode()
+      {
+        return (x, y).GetHashCode();
+      }
+
+      public static Coordinate2D operator +(Coordinate2D a) => a;
+      public static Coordinate2D operator -(Coordinate2D a) => new Coordinate2D(-a.x, -a.y);
+      public static Coordinate2D operator +(Coordinate2D a, Coordinate2D b) => new Coordinate2D(a.x + b.x, a.y + b.y);
+      public static Coordinate2D operator -(Coordinate2D a, Coordinate2D b) => a + (-b);
+
+      public static implicit operator (int x, int y)(Coordinate2D c) => (c.x, c.y);
+      public static implicit operator Coordinate2D((int x, int y) tuple) => new Coordinate2D(tuple.x, tuple.y);
     }
   }
 }
--- src/Puzzles/2021/CSharp/Solution_2021_21.cs
+++ src/Puzzles/2021/CSharp/Solution_2021_21.cs
@@ -7,10 +7,94 @@ namespace AdventOfCode.Solutions
   {
     public override void Solve(PuzzleInput input)
     {
-      // Write your puzzle solution here!
+      var lines = input.GetLines();
 
-      SubmitPartOne(string.Empty);
-      SubmitPartTwo(string.Empty);
+      int aStart = lines[0][lines[0].Length - 1] - '0';
+      int bStart = lines[1][lines[1].Length - 1] - '0';
+
+      int aScore = 0;
+      int bScore = 0;
+
+      int round = 0;
+
+      while (aScore < 1000 && bScore < 1000)
+      {
+        round++;
+        aScore += 1 + (aStart + 9 * round * round - 3 * round - 1) % 10;
+        bScore += aScore < 1000 ? 1 + (bStart + 9 * (round * round + round) - 3 * round - 1) % 10 : 0;
+      }
+
+      int rolls = (aScore < 1000 ? 6 * round : 6 * round - 3);
+      int score = rolls * (aScore < 1000 ? aScore : bScore);
+
+      SubmitPartOne(score);
+
+      Dictionary<int, int> rollCounts = new Dictionary<int, int>();
+      rollCounts[3] = 1;
+      rollCounts[4] = 3;
+      rollCounts[5] = 6;
+      rollCounts[6] = 7;
+      rollCounts[7] = 6;
+      rollCounts[8] = 3;
+      rollCounts[9] = 1;
+
+      Dictionary<(int aPos, int aScore, int bPos, int bScore), ulong> oldScoreCount = new Dictionary<(int, int, int, int), ulong>();
+      oldScoreCount[(aStart, 0, bStart, 0)] = 1;
+
+      bool isPlayerA = true;
+      ulong aWins = 0;
+      ulong bWins = 0;
+
+      while (oldScoreCount.Count > 0)
+      {
+        Dictionary<(int aPos, int aScore, int bPos, int bScore), ulong> newScoreCount = new Dictionary<(int, int, int, int), ulong>();
+
+        foreach (var key in oldScoreCount.Keys)
+        {
+          foreach (var roll in rollCounts.Keys)
+          {
+            if (isPlayerA)
+            {
+              int newPos = ((key.aPos + roll - 1) % 10) + 1;
+              int newScore = key.aScore + newPos;
+              ulong newCount = (ulong)oldScoreCount[key] * (ulong)rollCounts[roll];
+
+              if (newScore < 21)
+              {
+                var newKey = (newPos, newScore, key.bPos, key.bScore);
+                newScoreCount[newKey] = newScoreCount.GetValueOrDefault(newKey, 0UL) + (ulong)newCount;
+              }
+              else
+              {
+                aWins += newCount;
+              }
+            }
+            else
+            {
+              int newPos = ((key.bPos + roll - 1) % 10) + 1;
+              int newScore = key.bScore + newPos;
+              ulong newCount = (ulong)oldScoreCount[key] * (ulong)rollCounts[roll];
+
+              if (newScore < 21)
+              {
+                var newKey = (key.aPos, key.aScore, newPos, newScore);
+                newScoreCount[newKey] = newScoreCount.GetValueOrDefault(newKey, 0UL) + newCount;
+              }
+              else
+              {
+                bWins += newCount;
+              }
+            }
+          }
+        }
+
+        oldScoreCount = newScoreCount;
+        isPlayerA = !isPlayerA;
+      }
+
+      SubmitPartTwo(aWins > bWins ? aWins : bWins);
     }
   }
+
+
 }
